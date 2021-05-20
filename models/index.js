9350faf1df83d0d52f810063260e66aeb154d0f8
Hawa <hawa.francis@gmail.com>
2021-05-19 23:53:51 -0400

    index.js

--- models/index.js
+++ models/index.js
@@ -5,13 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+product.belongsTo(Category, {
+foreignKey: "category_id"
+});
 // Categories have many Products
-
+Category.hasMany(product, {
+  foreignKey: "category_id"
+});
 // Products belongToMany Tags (through ProductTag)
+product.belongtoMany(tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
 
+});
 // Tags belongToMany Products (through ProductTag)
-
+tag.belongToMany (product, {
+  through: ProductTag,
+  foreignKey: "tag_id"
+})
 module.exports = {
   Product,
   Category,

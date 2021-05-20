1b3b864483e88db23cf6bfbd93ad9cc636adc635
Hawa <hawa.francis@gmail.com>
2021-05-20 00:47:00 -0400

    category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,24 +5,111 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+// be sure to include its associated Products
+    include:
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"]
+       }
+    })
+    .then (dbCategoryData => {
+      if (!CategoryData) {
+        res.status(404).json({ message: 'No user with this id!' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
+  
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.perams.id
+    },
+     // be sure to include its associated Products
+    include: {
+      model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"]
+    }
+  })
+  .then (dbCategoryData => {
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.create_name
+  })
+  .then (dbCategoryData => {
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.perams.id
+    }
+  })
+  .then (dbCategoryData => {
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.perams.id
+    }
+  })
+  .then (dbCategoryData => {
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;

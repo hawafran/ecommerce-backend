5eb2e772ce32ffabe86facdd1a2ebc65c4b4e532
Hawa <hawa.francis@gmail.com>
2021-05-20 01:36:05 -0400

    tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,108 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll({
   // be sure to include its associated Product data
+  include:
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"]
+       }
+})
+.then (dbTagData => {
+  if (!CategoryData) {
+    res.status(404).json({ message: 'No user with this id!' });
+    return;
+  }
+  res.json(dbTagData);
+})
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
+});
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where:{
+      id:req.perams.id
+    },
+   // be sure to include its associated Product data
+   include: {
+    model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"]
+    }
+  })  
+  .then (dbTagData => {
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then (dbTagData => {
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.perams.id
+    }
+  })
+  .then (dbTagData => {
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.perams.id
+    }
+  })
+  .then (dbTagData => {
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
